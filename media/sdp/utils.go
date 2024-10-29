1d8026cea4cb4417bb9bf7ef22f40d27898197c2
Emir Aganovic <emirfreelance91@gmail.com>
2024-10-29 20:43:32 +0100

    fix: avoid int64 conversion on sdp

--- media/sdp/utils.go
+++ media/sdp/utils.go
@@ -11,15 +11,15 @@ import (
 )
 
 func GetCurrentNTPTimestamp() uint64 {
-	ntpEpochOffset := 2208988800 // Offset from Unix epoch (January 1, 1970) to NTP epoch (January 1, 1900)
+	var ntpEpochOffset int64 = 2208988800 // Offset from Unix epoch (January 1, 1970) to NTP epoch (January 1, 1900)
 	currentTime := time.Now().Unix() + int64(ntpEpochOffset)
 
 	return uint64(currentTime)
 }
 
 func NTPTimestamp(now time.Time) uint64 {
-	ntpEpochOffset := 2208988800 // Offset from Unix epoch (January 1, 1970) to NTP epoch (January 1, 1900)
-	currentTime := now.Unix() + int64(ntpEpochOffset)
+	var ntpEpochOffset int64 = 2208988800 // Offset from Unix epoch (January 1, 1970) to NTP epoch (January 1, 1900)
+	currentTime := now.Unix() + ntpEpochOffset
 
 	return uint64(currentTime)
 }
