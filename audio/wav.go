317ace2b805252ce24aba7c66dbe0c66c92b7c8f
Emir Aganovic <emirfreelance91@gmail.com>
2024-12-14 22:25:27 +0100

    feat: remove WavDecoder as it is used as WavReader

--- audio/wav.go
+++ audio/wav.go
@@ -6,44 +6,7 @@ package audio
 import (
 	"encoding/binary"
 	"io"
-
-	"github.com/go-audio/riff"
-	"github.com/go-audio/wav"
 )
-
-func NewWavDecoder(r io.ReadSeeker) *wav.Decoder {
-	dec := wav.NewDecoder(r)
-	return dec
-}
-
-// Decoder handles the decoding of wav files.
-type Decoder struct {
-	*riff.Parser
-}
-
-func NewWavDecoderStreamer(r io.Reader) *Decoder {
-	return &Decoder{
-		riff.New(r),
-	}
-}
-
-func (d *Decoder) ReadHeaders() (err error) {
-	var chunk *riff.Chunk
-
-	d.ParseHeaders()
-	for err == nil {
-		chunk, err = d.NextChunk()
-		if err != nil {
-			break
-		}
-
-		if chunk.ID == riff.FmtID {
-			chunk.DecodeWavHeader(d.Parser)
-			break
-		}
-	}
-	return err
-}
 
 // WavWriteVoipPCM is normally 16 bit mono 8000 PCM
 func WavWriteVoipPCM(w io.Writer, audio []byte) (int, error) {
