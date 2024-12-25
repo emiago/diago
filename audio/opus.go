ca4c2b99566bb66169ddaa569c161a0b11096962
Emir Aganovic <emirfreelance91@gmail.com>
2024-12-25 17:33:47 +0100

    fix: remove unused opus code

--- audio/opus.go
+++ audio/opus.go
@@ -16,24 +16,12 @@ type OpusEncoder struct {
 }
 
 func (enc *OpusEncoder) EncodeTo(data []byte, lpcm []byte) (int, error) {
-	// What if lpcm is larger than our frame?
-	if len(lpcm)*2 > len(enc.pcmInt16) {
-
-	}
-
-	step := min(len(enc.pcmInt16), len(lpcm)*2)
-	for i := 0; i < len(lpcm); i = i + step {
-
-	}
-
 	n, err := samplesByteToInt16(lpcm, enc.pcmInt16)
 	if err != nil {
 		return 0, err
 	}
 
 	// NOTE: opus has fixed frame sizes 2.5, 5, 10, 20, 40 or 60 ms
-	// frameSize := 960
-	// maxN := min(frameSize, n)
 	pcmInt16 := enc.pcmInt16[:n]
 
 	n, err = enc.Encode(pcmInt16, data)
@@ -44,31 +32,10 @@ func (enc *OpusEncoder) EncodeTo(data []byte, lpcm []byte) (int, error) {
 type OpusDecoder struct {
 	opus.Decoder
 	pcmInt16    []int16
-	unread      int
-	off         int
 	numChannels int
 }
 
 func (dec *OpusDecoder) DecodeTo(lpcm []byte, data []byte) (int, error) {
-	// For now we do not track unread. Should be caller who needs to watch out on codec sample size
-	// if data == nil {
-	// 	if dec.unread > 0 {
-	// 		// log.Trace().Int("n", dec.unread).Msg("opus: returning unread")
-
-	// 		n := dec.unread
-	// 		off := dec.off
-
-	// 		maxRead := min(len(lpcm)/2, n)
-	// 		dec.unread = n - maxRead
-	// 		dec.off += maxRead
-
-	// 		pcm := dec.pcmInt16[off : off+maxRead]
-	// 		n, err := samplesInt16ToBytes(pcm, lpcm)
-	// 		return n, err
-	// 	}
-	// 	return 0, nil
-	// }
-
 	pcmN, err := dec.Decoder.Decode(data, dec.pcmInt16)
 	if err != nil {
 		return 0, err
@@ -79,10 +46,6 @@ func (dec *OpusDecoder) DecodeTo(lpcm []byte, data []byte) (int, error) {
 		// Should never happen
 		return 0, fmt.Errorf("opus: pcm int buffer expected=%d", pcmN)
 	}
-
-	// maxRead := min(len(lpcm)/2, pcmN)
-	// dec.unread = pcmN - maxRead
-	// dec.off = maxRead
 
 	pcm := dec.pcmInt16[:pcmN]
 	n, err := samplesInt16ToBytes(pcm, lpcm)
