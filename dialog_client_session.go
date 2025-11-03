2cfdf41f2085c8ab2595229358bac0da940fcfef
Alexandr Danilin <alexandr.danilin@ooma.com>
2025-11-04 00:09:21 +0300

    Fixes #105

--- dialog_client_session.go
+++ dialog_client_session.go
@@ -213,7 +213,8 @@ func (d *DialogClientSession) Invite(ctx context.Context, opts InviteClientOptio
 	inviteReq.SetBody(sess.LocalSDP())
 
 	// We allow changing full from header, but we need to make sure it is correctly set
-	if fromHDR := inviteReq.From(); fromHDR != nil {
+	// If users specify 'tag' parameter it is assumed that they know what they do
+	if fromHDR := inviteReq.From(); fromHDR != nil && !fromHDR.Params.Has("tag") {
 		fromHDR.Params["tag"] = sip.GenerateTagN(16)
 	}
 
