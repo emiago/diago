dd1bb4b7e3c88e3b799e544b60eee4ed6e802f35
Emir Aganovic <emirfreelance91@gmail.com>
2026-08-02 17:55:14 +0200

    feat(diago): add direct VoIP WebRTC dialogs

---
+++ voip_webrtc.go
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MPL-2.0
+// SPDX-FileCopyrightText: Copyright (c) 2026, Emir Aganovic
+
+package diago
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"fmt"
+	"math/big"
+	"time"
+
+	"github.com/emiago/diago/media"
+)
+
+func prepareVoipWebrtcConfig(conf media.MediaSessionWebrtcConfig) (media.MediaSessionWebrtcConfig, error) {
+	if len(conf.DTLS.Certificates) != 0 {
+		return conf, nil
+	}
+	certificate, err := generateVoipWebrtcCertificate()
+	if err != nil {
+		return conf, fmt.Errorf("generate WebRTC DTLS certificate: %w", err)
+	}
+	conf.DTLS.Certificates = []tls.Certificate{certificate}
+	return conf, nil
+}
+
+func generateVoipWebrtcCertificate() (tls.Certificate, error) {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return tls.Certificate{}, err
+	}
+	serialLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serial, err := rand.Int(rand.Reader, serialLimit)
+	if err != nil {
+		return tls.Certificate{}, err
+	}
+	now := time.Now()
+	template := &x509.Certificate{
+		SerialNumber: serial,
+		Subject:      pkix.Name{CommonName: "diago WebRTC"},
+		NotBefore:    now.Add(-time.Minute),
+		NotAfter:     now.Add(24 * time.Hour),
+		KeyUsage:     x509.KeyUsageDigitalSignature,
+		ExtKeyUsage: []x509.ExtKeyUsage{
+			x509.ExtKeyUsageClientAuth,
+			x509.ExtKeyUsageServerAuth,
+		},
+		BasicConstraintsValid: true,
+	}
+	der, err := x509.CreateCertificate(rand.Reader, template, template, &privateKey.PublicKey, privateKey)
+	if err != nil {
+		return tls.Certificate{}, err
+	}
+	template.Raw = der
+	return tls.Certificate{
+		Certificate: [][]byte{der},
+		PrivateKey:  privateKey,
+		Leaf:        template,
+	}, nil
+}
